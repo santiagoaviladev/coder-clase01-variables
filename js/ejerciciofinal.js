2a9920048bc5d0aa3e110794319373a7b2776cde
Santiago Avila <xavier0823@gmail.com>
2022-04-21 18:57:06 -0500

    ejercicio final resuelta

--- js/ejerciciofinal.js
+++ js/ejerciciofinal.js
@@ -1,3 +1,20 @@
 /* Escriba un programa que le pida a un usuario Su nombre y 
 2 números y muestre un saludo por consola, y el resultado de la suma de 
 los dos números, en un alert*/
+
+console.log("***EJERCICIO FINAL***");
+
+/*
+    1. Pedirle al usuario que ingrese un nombre
+    2. Pedirle al usuario que ingrese un numero
+    3. Pedirle al usuario que ingrese otro numero 
+    4. Imprimir la suma de los numeros en ALERT
+    5. Imprimer saludo por consola
+
+*/
+
+let nombreUsuario = prompt("INGRESE SU NOMBRE");
+let finalNumero1 = Number(prompt("Ingrese un numero"));
+let finalNumero2 = Number(prompt("Ingrese otro numero"));
+alert(finalNumero1+finalNumero2);
+console.log("HOLA", nombreUsuario);
